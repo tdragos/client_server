8da23a91894339e47b3e3b8c350a24d24f16f613
Dragos Tudor <drtudor@luxoft.com>
2019-08-21 15:24:57 +0300

    Works for 1 client TODO: implement select on server to accept multiple clients TODO: get functionality for ipv6 too

--- client/client.c
+++ client/client.c
@@ -1,6 +1,93 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+#define ADDR "127.0.0.1"
+#define PORT 4445
+#define BUFSIZE 1000
 
 int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        printf("Usage is ./client [FILENAME]");
+        exit(1);
+    }
+    
+    int sockfd, status;
+    struct sockaddr_in sa;
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("create socket");
+        exit(1);
+    }
+    
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(PORT);
+    inet_aton(ADDR, &sa.sin_addr);
+    
+    //connect to the server
+    status = connect(sockfd, (const struct sockaddr *)&sa, sizeof(struct sockaddr_in));
+    if (status < 0) {
+        perror("connect");
+        exit(1);
+    }
+    
+    //send the filename to the server
+    status = write(sockfd, argv[1], strlen(argv[1]));
+    if (status < 0) {
+        perror("write");
+        exit(1);
+    }
+    
+    //check if the server has the file
+    int chars_read, chars_written;
+    char buf[BUFSIZE];
+    chars_read = recv(sockfd, buf, 14, 0);
+    if (!strncmp(buf, "File not found", 14)) {
+        printf("File not found");
+        return 0;
+    }
+    
+    //receive the file
+    //we use select to see when we get data from the server
+    //we set a timeout of 1 second
+    int openfd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    struct timeval tm;
+    tm.tv_sec = 1;
+    tm.tv_usec = 0;
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(sockfd, &fds);
+    
+    while (1) {
+        status = select(sockfd + 1, &fds, NULL, NULL, &tm);
+        if (status == -1) {
+            perror("select");
+            close(openfd);
+            return 0;
+        }
+        else if (status) {
+            chars_read = recv(sockfd, buf, BUFSIZE, 0);
+            if (chars_read > 0)
+                write(openfd, buf, chars_read);
+        }
+        else if (status == 0)
+            //timeout => file was received so we exit the loop
+            break;
+    }
+    
+    //close the file
+    close(openfd);
+    
+    printf("Received the file\n");
+    
     return 0;
 }
--- server/server.c
+++ server/server.c
@@ -1,7 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+#define ADDR "127.0.0.1"
+#define MAX_CLIENTS 5
+#define BASE 10
+#define BUFSIZE 1000
 
 int main(int argc, char **argv)
 {
-    printf("%s\n", argv[1]);
+    if (argc < 2) {
+        printf("Usage is ./server PORT\n");
+        exit(1);
+    }
+    
+    int sockfd, status, connection_socket;
+    char *ptr;
+    struct sockaddr_in sa;
+
+    //create the socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("create socket");
+        exit(1);
+    }
+
+    sa.sin_family = AF_INET;
+    inet_aton(ADDR, &sa.sin_addr);
+    //get the port from argv[1] with strtol (atoi is deprecated)
+    sa.sin_port = htons((unsigned short)strtol(argv[1], &ptr, BASE));
+
+    status = bind(sockfd, (struct sockaddr *)&sa, sizeof(struct sockaddr_in));
+    if (status < 0) {
+        perror("bind");
+        exit(1);
+    }
+
+    status = listen(sockfd, MAX_CLIENTS);
+    if (status < 0) {
+        perror("listen");
+        exit(1);
+    }
+    
+
+    //server waits for clients forever
+    while (1) {
+        //accept a connection
+        connection_socket = accept(sockfd, NULL, NULL);
+        if (connection_socket < 0) {
+            perror("accept");
+            exit(1);
+        }
+
+        //read the filename. We assume it's length is max 20
+        int chars_read, chars_written;
+        char filename[20];
+        
+        chars_read = read(connection_socket, filename, 20);
+        if (chars_read <= 0) {
+            perror("error receiving filename, we tell the client we don't have the file");
+            write(connection_socket, "File not found", 14);
+            continue;
+        }
+        filename[chars_read] = '\0';
+        
+        //tell the client we have the file
+        chars_written = send(connection_socket, "File was found", 14, 0);
+        
+        //open the file
+        int open_fd = open(filename, O_RDONLY);
+        if (open_fd < 0) {
+            //if open fails it means we don't have the file
+            perror("we don't have the file");
+            write (connection_socket, "File not found", 14);
+            continue;
+        }
+        
+        //send the file
+        char buf[BUFSIZE];
+        while ((chars_read = read(open_fd, buf, BUFSIZE)) > 0) {
+            chars_written = send(connection_socket, buf, chars_read, 0);
+        }
+
+        //close the file
+        close(open_fd);
+        
+        printf("Send was successful\n");
+    }
+    
     return 0;
 }
